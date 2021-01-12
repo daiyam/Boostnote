b747db39dfc70c8ed57eda2a019ef74bc78cbfb5
Baptiste Augrain <daiyam@zokugun.org>
2021-01-12 19:03:30 +0100

    improve tag input

--- browser/main/Detail/TagSelect.js
+++ browser/main/Detail/TagSelect.js
@@ -9,282 +9,357 @@ import Autosuggest from 'react-autosuggest'
 import { TagQuery } from 'browser/main/lib/TagQuery'
 
 class TagSelect extends React.Component {
-  constructor (props) {
-    super(props)
+	constructor(props) {
+		super(props)
 
-    this.state = {
-      newTag: '',
-      suggestions: [],
-      mouseOverTag: null
-    }
+		this.state = {
+			newTag: '',
+			suggestions: [],
+			mouseOverTag: null,
+			contextMenu: false,
+		}
 
-    this.handleAddTag = this.handleAddTag.bind(this)
-    this.onInputBlur = this.onInputBlur.bind(this)
-    this.onInputChange = this.onInputChange.bind(this)
-    this.onInputKeyDown = this.onInputKeyDown.bind(this)
-    this.onSuggestionsClearRequested = this.onSuggestionsClearRequested.bind(this)
-    this.onSuggestionsFetchRequested = this.onSuggestionsFetchRequested.bind(this)
-    this.onSuggestionSelected = this.onSuggestionSelected.bind(this)
-  }
+		this.handleAddTag = this.handleAddTag.bind(this)
+		this.onContextMenu = this.onContextMenu.bind(this)
+		this.onInputBlur = this.onInputBlur.bind(this)
+		this.onInputChange = this.onInputChange.bind(this)
+		this.onInputKeyDown = this.onInputKeyDown.bind(this)
+		this.onSuggestionsClearRequested = this.onSuggestionsClearRequested.bind(this)
+		this.onSuggestionsFetchRequested = this.onSuggestionsFetchRequested.bind(this)
+		this.onSuggestionSelected = this.onSuggestionSelected.bind(this)
+		this.shouldRenderSuggestions = this.shouldRenderSuggestions.bind(this)
+	}
 
-  addNewTag (newTag) {
-    newTag = newTag.trim().replace(/ +/g, '_')
-    if (newTag.charAt(0) === '#') {
-      newTag.substring(1)
-    }
+	addNewTag(newTag) {
+		newTag = newTag.trim().replace(/ +/g, '_')
+		if (newTag.charAt(0) === '#') {
+			newTag.substring(1)
+		}
 
-    if (newTag.length <= 0) {
-      this.setState({
-        newTag: ''
-      })
-      return
-    }
+		if (newTag.length <= 0) {
+			this.setState({
+				newTag: ''
+			})
+			return
+		}
 
-    let { value } = this.props
-    value = _.isArray(value)
-      ? value.slice()
-      : []
-    value.push(newTag)
-    value = _.uniq(value)
+		let { value } = this.props
+		value = _.isArray(value)
+			? value.slice()
+			: []
+		value.push(newTag)
+		value = _.uniq(value)
 
-    this.setState({
-      newTag: ''
-    }, () => {
-      this.value = value
-      this.props.onChange()
-    })
-  }
+		this.setState({
+			newTag: ''
+		}, () => {
+			this.value = value
+			this.props.onChange()
+		})
+	}
 
-  buildSuggestions () {
-    const { data, location } = this.props
+	buildSuggestions() {
+		const { data, location } = this.props
 
-    let tagList
-    if (typeof location.query.storage !== 'undefined' && location.query.storage !== '') {
-      const id = location.query.storage.substr(1)
+		let tagList
+		if (typeof location.query.storage !== 'undefined' && location.query.storage !== '') {
+			const id = location.query.storage.substr(1)
 
-      let noteMap
-      if (location.query.storage[0] === 's') {
-        noteMap = data.noteMap.filter(note => note.storage === id)
-      } else {
-        noteMap = data.noteMap.filter(note => note.folder === id)
-      }
+			let noteMap
+			if (location.query.storage[0] === 's') {
+				noteMap = data.noteMap.filter(note => note.storage === id)
+			} else {
+				noteMap = data.noteMap.filter(note => note.folder === id)
+			}
 
-      tagList = []
+			tagList = []
 
-      const tags = {}
+			const tags = {}
 
-      noteMap.forEach(note => {
-        note.tags.forEach(name => {
-          if (tags[name]) {
-            tags[name].size++
-          } else {
-            const tag = { name, nameLC: name.toLowerCase(), size: 1 }
+			noteMap.forEach(note => {
+				note.tags.forEach(name => {
+					if (tags[name]) {
+						tags[name].size++
+					} else {
+						const tag = { name, nameLC: name.toLowerCase(), size: 1 }
 
-            tags[name] = tag
+						tags[name] = tag
 
-            tagList.push(tag)
-          }
-        })
-      })
-    } else {
-       tagList = data.tagNoteMap.map(
-        (tag, name) => ({ name, nameLC: name.toLowerCase(), size: tag.size })
-      ).filter(
-        tag => tag.size > 0
-      )
-    }
+						tagList.push(tag)
+					}
+				})
+			})
+		} else {
+			tagList = data.tagNoteMap.map(
+				(tag, name) => ({ name, nameLC: name.toLowerCase(), size: tag.size })
+			).filter(
+				tag => tag.size > 0
+			)
+		}
 
-    this.suggestions = _.sortBy(tagList, ['name'])
-    this.storage = location.query.storage || ''
-  }
+		this.suggestions = _.sortBy(tagList, ['name'])
+		this.storage = location.query.storage || ''
+	}
 
-  componentDidMount () {
-    this.value = this.props.value
+	componentDidMount() {
+		this.value = this.props.value
 
-    this.buildSuggestions()
+		this.buildSuggestions()
 
-    ee.on('editor:add-tag', this.handleAddTag)
-  }
+		ee.on('editor:add-tag', this.handleAddTag)
+	}
 
-  componentDidUpdate () {
-    this.value = this.props.value
+	componentDidUpdate() {
+		this.value = this.props.value
 
-     if (this.storage !== this.props.location.query.storage) {
-      this.buildSuggestions()
-     }
-  }
+		if (this.storage !== this.props.location.query.storage) {
+			this.buildSuggestions()
+		}
+	}
 
-  componentWillUnmount () {
-    ee.off('editor:add-tag', this.handleAddTag)
-  }
+	componentWillUnmount() {
+		ee.off('editor:add-tag', this.handleAddTag)
+	}
 
-  handleAddTag () {
-    this.refs.newTag.input.focus()
-  }
+	handleAddTag() {
+		this.refs.newTag.input.focus()
+	}
 
-  handleTagLabelClick (tag) {
-    const { router } = this.context
-    const { location } = this.props
+	handleTagLabelClick(tag) {
+		const { router } = this.context
+		const { location } = this.props
 
-    TagQuery.navigate(tag, location, router)
-  }
+		TagQuery.navigate(tag, location, router)
+	}
 
-  handleTagRemoveButtonClick (tag) {
-    this.removeTagByCallback((value, tag) => {
-      value.splice(value.indexOf(tag), 1)
-    }, tag)
-  }
+	handleTagRemoveButtonClick(tag) {
+		this.removeTagByCallback((value, tag) => {
+			value.splice(value.indexOf(tag), 1)
+		}, tag)
 
-  onInputBlur (e) {
-    this.submitNewTag()
-  }
+		this.setState({
+			mouseOverTag: ''
+		})
+	}
 
-  onInputChange (e, { newValue, method }) {
-    this.setState({
-      newTag: newValue
-    })
-  }
+	onContextMenu(e) {
+		const { data, location } = this.props
+		const filter = e.altKey ? (name) => !_.includes(this.value, name) : (name) => !_.includes(this.value, name) && name.split('').every(c => c.charCodeAt(0) > 128)
 
-  onInputKeyDown (e) {
-    switch (e.keyCode) {
-      case 9:
-        e.preventDefault()
-        this.submitNewTag()
-        break
-      case 13:
-        this.submitNewTag()
-        break
-      case 8:
-        if (this.state.newTag.length === 0) {
-          this.removeLastTag()
-        }
-    }
-  }
+		const tagList = []
 
-  onSuggestionsClearRequested () {
-    this.setState({
-      suggestions: []
-    })
-  }
+		if (typeof location.query.storage !== 'undefined' && location.query.storage !== '') {
+			const id = location.query.storage.substr(1)
 
-  onSuggestionsFetchRequested ({ value }) {
-    const valueLC = value.toLowerCase()
-    const suggestions = _.filter(
-      this.suggestions,
-      tag => !_.includes(this.value, tag.name) && tag.nameLC.indexOf(valueLC) !== -1
-    )
+			let noteMap
+			if (location.query.storage[0] === 's') {
+				noteMap = data.noteMap.filter(note => note.storage === id)
+			}
+			else {
+				noteMap = data.noteMap.filter(note => note.folder === id)
+			}
 
-    this.setState({
-      suggestions
-    })
-  }
+			const tags = {}
 
-  onSuggestionSelected (event, { suggestion, suggestionValue }) {
-    this.addNewTag(suggestionValue)
-  }
+			noteMap.forEach(note => {
+				note.tags.forEach(name => {
+					if (tags[name]) {
+						tags[name].size++
+					}
+					else if (filter(name)) {
+						const tag = { name, nameLC: name.toLowerCase(), size: 1 }
 
-  removeLastTag () {
-    this.removeTagByCallback((value) => {
-      value.pop()
-    })
-  }
+						tags[name] = tag
 
-  removeTagByCallback (callback, tag = null) {
-    let { value } = this.props
+						tagList.push(tag)
+					}
+				})
+			})
+		}
+		else {
+			data.tagNoteMap.forEach((tag, name) => {
+				if (tag.size > 0 && filter(name)) {
+					tagList.push({ name, nameLC: name.toLowerCase(), size: tag.size })
+				}
+			})
+		}
 
-    value = _.isArray(value)
-      ? value.slice()
-      : []
-    callback(value, tag)
-    value = _.uniq(value)
+		this.setState({
+			contextMenu: true,
+			suggestions: _.sortBy(tagList, ['name'])
+		})
+	}
 
-    this.value = value
-    this.props.onChange()
-  }
+	onInputBlur(e) {
+		this.submitNewTag()
+	}
 
-  reset () {
-    this.buildSuggestions()
+	onInputChange(e, { newValue, method }) {
+		this.setState({
+			newTag: newValue,
+			contextMenu: false
+		})
+	}
 
-    this.setState({
-      newTag: ''
-    })
-  }
+	onInputKeyDown(e) {
+		switch (e.keyCode) {
+			case 9:
+				e.preventDefault()
+				this.submitNewTag()
+				break
+			case 13:
+				this.submitNewTag()
+				break
+			default:
+				if (this.suggestions.length !== 0 && this.state.suggestions.length === 0) {
+					this.setState({
+						suggestions: _.filter(
+							this.suggestions,
+							tag => !_.includes(this.value, tag.name)
+						)
+					})
+				}
+		}
+	}
 
-  submitNewTag () {
-    this.addNewTag(this.refs.newTag.input.value)
-  }
+	onSuggestionsClearRequested() {
+		this.setState({
+			suggestions: [],
+			contextMenu: false
+		})
+	}
 
-  setMouseOver(tag) {
-    this.setState({
-      mouseOverTag: tag
-    })
-  }
+	onSuggestionsFetchRequested({ value }) {
+		const valueLC = value.toLowerCase()
+		const suggestions = _.filter(
+			this.suggestions,
+			tag => !_.includes(this.value, tag.name) && tag.nameLC.indexOf(valueLC) !== -1
+		)
 
-  render () {
-    const { value, className, showTagsAlphabetically } = this.props
-    const { newTag, suggestions, mouseOverTag } = this.state
+		this.setState({
+			suggestions
+		})
+	}
 
-    const tagList = _.isArray(value)
-      ? (showTagsAlphabetically ? _.sortBy(value) : value).map((tag) => {
-        return (
-          <span styleName='tag'
-            key={tag}
-            className={mouseOverTag === tag ? 'TagSelect__over___browser-main-Detail-' : ''}
-          >
-            <span styleName='tag-label' onClick={(e) => this.handleTagLabelClick(tag)}>#{tag}</span>
-            <button styleName='tag-removeButton'
-              onClick={(e) => this.handleTagRemoveButtonClick(tag)}
-              onMouseEnter={(e) => this.setMouseOver(tag)}
-              onMouseLeave={(e) => this.setMouseOver(null)}
-            >
-              <img className='tag-removeButton-icon' src='../resources/icon/icon-x.svg' width='8px' />
-            </button>
-          </span>
-        )
-      })
-      : []
+	onSuggestionSelected(event, { suggestion, suggestionValue }) {
+		this.addNewTag(suggestionValue)
 
-    return (
-      <div className={_.isString(className)
-          ? 'TagSelect ' + className
-          : 'TagSelect'
-        }
-        styleName='root'
-      >
-        {tagList}
-        <Autosuggest
-          ref='newTag'
-          suggestions={suggestions}
-          onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
-          onSuggestionsClearRequested={this.onSuggestionsClearRequested}
-          onSuggestionSelected={this.onSuggestionSelected}
-          getSuggestionValue={suggestion => suggestion.name}
-          renderSuggestion={suggestion => (
-            <div>
-              {suggestion.name}
-            </div>
-          )}
-          inputProps={{
-            placeholder: i18n.__('Add tag...'),
-            value: newTag,
-            onChange: this.onInputChange,
-            onKeyDown: this.onInputKeyDown,
-            onBlur: this.onInputBlur
-          }}
-        />
-      </div>
-    )
-  }
+		this.setState({
+			contextMenu: false
+		})
+	}
+
+	removeLastTag() {
+		this.removeTagByCallback((value) => {
+			value.pop()
+		})
+	}
+
+	removeTagByCallback(callback, tag = null) {
+		let { value } = this.props
+
+		value = _.isArray(value)
+			? value.slice()
+			: []
+		callback(value, tag)
+		value = _.uniq(value)
+
+		this.value = value
+		this.props.onChange()
+	}
+
+	reset() {
+		this.buildSuggestions()
+
+		this.setState({
+			newTag: ''
+		})
+	}
+
+	setMouseOver(tag) {
+		this.setState({
+			mouseOverTag: tag
+		})
+	}
+
+	shouldRenderSuggestions(value) {
+		if(this.state.suggestions.length === 0) {
+			return false
+		}
+		if(value.length === 0 && !this.state.contextMenu) {
+			return false
+		}
+
+		return true
+	}
+
+	submitNewTag() {
+		this.addNewTag(this.refs.newTag.input.value)
+	}
+
+	render() {
+		const { value, className, showTagsAlphabetically } = this.props
+		const { newTag, suggestions, mouseOverTag } = this.state
+
+		const tagList = _.isArray(value)
+			? (showTagsAlphabetically ? _.sortBy(value) : value).map((tag) => {
+				return (
+					<span styleName='tag'
+						key={tag}
+						className={mouseOverTag === tag ? 'TagSelect__over___browser-main-Detail-' : ''}
+					>
+						<span styleName='tag-label' onClick={(e) => this.handleTagLabelClick(tag)}>#{tag}</span>
+						<button styleName='tag-removeButton'
+							onClick={(e) => this.handleTagRemoveButtonClick(tag)}
+							onMouseEnter={(e) => this.setMouseOver(tag)}
+							onMouseLeave={(e) => this.setMouseOver(null)}
+						>
+							<img className='tag-removeButton-icon' src='../resources/icon/icon-x.svg' width='8px' />
+						</button>
+					</span>
+				)
+			})
+			: []
+
+		return (
+			<div className={_.isString(className)
+				? 'TagSelect ' + className
+				: 'TagSelect'
+			}
+				styleName='root'
+			>
+				{tagList}
+				<Autosuggest
+					ref='newTag'
+					suggestions={suggestions}
+					onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+					onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+					onSuggestionSelected={this.onSuggestionSelected}
+					getSuggestionValue={suggestion => suggestion.name}
+					shouldRenderSuggestions={this.shouldRenderSuggestions}
+					renderSuggestion={suggestion => <div>{suggestion.name}</div>}
+					inputProps={{
+						placeholder: i18n.__('Add tag...'),
+						value: newTag,
+						onChange: this.onInputChange,
+						onKeyDown: this.onInputKeyDown,
+						onBlur: this.onInputBlur,
+						onContextMenu: this.onContextMenu
+					}}
+				/>
+			</div>
+		)
+	}
 }
 
 TagSelect.contextTypes = {
-  router: PropTypes.shape({})
+	router: PropTypes.shape({})
 }
 
 TagSelect.propTypes = {
-  className: PropTypes.string,
-  value: PropTypes.arrayOf(PropTypes.string),
-  onChange: PropTypes.func
+	className: PropTypes.string,
+	value: PropTypes.arrayOf(PropTypes.string),
+	onChange: PropTypes.func
 }
 
 export default CSSModules(TagSelect, styles)
