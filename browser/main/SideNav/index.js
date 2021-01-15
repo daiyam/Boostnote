719b57f0d936fb313b899f4bbda2b19ca38236a6
Baptiste Augrain <daiyam@zokugun.org>
2021-01-15 04:02:52 +0100

    filter tags by prefix

--- browser/main/SideNav/index.js
+++ browser/main/SideNav/index.js
@@ -24,17 +24,78 @@ import { confirmDeleteNote } from 'browser/lib/confirmDeleteNote'
 import { TagQuery, TagState } from 'browser/main/lib/TagQuery'
 
 class SideNav extends React.Component {
-	// TODO: should not use electron stuff v0.7
+	constructor(props) {
+		super(props)
 
-	componentDidMount() {
+		this.state = {
+			prefix: ''
+		}
+	}
+
+	componentDidMount() { // {{{
 		ee.on('side:preferences', this.handleMenuButtonClick)
-	}
+	} // }}}
 
-	componentWillUnmount() {
+	componentWillUnmount() { // {{{
 		ee.off('side:preferences', this.handleMenuButtonClick)
-	}
+	} // }}}
 
-	handleDeleteTagClick(tag) {
+	emptyTrash(entries) { // {{{
+		const { dispatch } = this.props
+		const deletionPromises = entries.map((note) => {
+			return dataApi.deleteNote(note.storage, note.key)
+		})
+		const { confirmDeletion } = this.props.config.ui
+		if (!confirmDeleteNote(confirmDeletion, true)) return
+		Promise.all(deletionPromises)
+			.then((arrayOfStorageAndNoteKeys) => {
+				arrayOfStorageAndNoteKeys.forEach(({ storageKey, noteKey }) => {
+					dispatch({ type: 'DELETE_NOTE', storageKey, noteKey })
+				})
+			})
+			.catch((err) => {
+				console.error('Cannot Delete note: ' + err)
+			})
+	} // }}}
+
+	handleClickTagListItem(tag) { // {{{
+		const { router } = this.context
+		const { location } = this.props
+
+		const query = new TagQuery().addPositiveTag(tag)
+
+		query.navigate(location, router)
+	} // }}}
+
+	handleClickNarrowToTag(tag, newState) { // {{{
+		const { router } = this.context
+		const { location } = this.props
+
+		const query = new TagQuery(location.pathname)
+
+		if (newState === TagState.NEGATIVE) {
+			if (!query.isNegativeTag(tag)) {
+				query.addNegativeTag(tag)
+			}
+		}
+		else if (newState === TagState.POSITIVE) {
+			if (!query.isPositiveTag(tag)) {
+				query.addPositiveTag(tag)
+			}
+		}
+		else {
+			if (query.isPositiveTag(tag)) {
+				query.removePositiveTag(tag)
+			}
+			else {
+				query.removeNegativeTag(tag)
+			}
+		}
+
+		query.navigate(location, router)
+	} // }}}
+
+	handleDeleteTagClick(tag) { // {{{
 		const selectedButton = remote.dialog.showMessageBox(remote.getCurrentWindow(), {
 			ype: 'warning',
 			message: i18n.__('Confirm tag deletion'),
@@ -70,24 +131,38 @@ class SideNav extends React.Component {
 					if (TagQuery.isTagQuery(location.pathname)) {
 						const query = new TagQuery(location.pathname)
 
-						if(query.removeTag(tag)) {
+						if (query.removeTag(tag)) {
 							query.navigate(location, this.context.router)
 						}
 					}
 				})
 		}
-	}
+	} // }}}
 
-	handleHomeButtonClick(e) {
+	handleFilterButtonContextMenu(event) { // {{{
+		const { data } = this.props
+		const trashedNotes = data.trashedSet.toJS().map((uniqueKey) => data.noteMap.get(uniqueKey))
+		context.popup([
+			{ label: i18n.__('Empty Trash'), click: () => this.emptyTrash(trashedNotes) }
+		])
+	} // }}}
+
+	handleHomeButtonClick(e) { // {{{
 		const { router } = this.context
 		router.push('/home')
-	}
+	} // }}}
 
-	handleMenuButtonClick(e) {
+	handleMenuButtonClick(e) { // {{{
 		openModal(PreferencesModal)
-	}
+	} // }}}
 
-	handleRenameTagClick(tagName) {
+	handlePrefixChange(e) { // {{{
+		this.setState({
+			prefix: e.target.value
+		})
+	} // }}}
+
+	handleRenameTagClick(tagName) { // {{{
 		const { data, dispatch, location, params } = this.props
 		const { router } = this.context
 
@@ -99,14 +174,76 @@ class SideNav extends React.Component {
 			params,
 			router
 		})
-	}
+	} // }}}
 
-	handleStarredButtonClick(e) {
+	handleStorageChange(e) { // {{{
+		const { router } = this.context
+		const { data, location } = this.props
+
+		const storage = e.target.value
+		const id = storage.substr(1)
+		const test = storage[0] === 'f' ? (note) => data.noteMap.get(note).folder === id : (note) => data.noteMap.get(note).storage === id
+
+		let key = location.query.key || ''
+		if (key && !test(key)) {
+			key = ''
+		}
+
+		const query = new TagQuery(location.pathname)
+
+		query.filter((tag) => data.tagNoteMap.get(tag).some((note) => test(note)))
+
+		const pathname = query.toPath()
+
+		router.push({
+			pathname,
+			query: {
+				key,
+				search: location.query.search || '',
+				storage
+			}
+		})
+	} // }}}
+
+	handleSortTagsByChange(e) { // {{{
+		const { dispatch } = this.props
+
+		const config = {
+			sortTagsBy: e.target.value
+		}
+
+		ConfigManager.set(config)
+		dispatch({
+			type: 'SET_CONFIG',
+			config
+		})
+	} // }}}
+
+	handleStarredButtonClick(e) { // {{{
 		const { router } = this.context
 		router.push('/starred')
-	}
+	} // }}}
 
-	handleTagContextMenu(e, tag) {
+	handleSwitchFoldersButtonClick() { // {{{
+		const { router } = this.context
+		router.push('/home')
+	} // }}}
+
+	handleSwitchTagsButtonClick() { // {{{
+		const { router } = this.context
+		const { location } = this.props
+
+		router.push({
+			pathname: `/alltags`,
+			query: {
+				key: location.query.key || '',
+				search: '',
+				storage: location.query.storage || ''
+			}
+		})
+	} // }}}
+
+	handleTagContextMenu(e, tag) { // {{{
 		context.popup([
 			{
 				label: i18n.__('Rename Tag'),
@@ -120,9 +257,9 @@ class SideNav extends React.Component {
 				click: this.handleDeleteTagClick.bind(this, tag)
 			}
 		])
-	}
+	} // }}}
 
-	handleToggleButtonClick(e) {
+	handleToggleButtonClick(e) { // {{{
 		const { dispatch, config } = this.props
 
 		ConfigManager.set({ isSideNavFolded: !config.isSideNavFolded })
@@ -130,33 +267,14 @@ class SideNav extends React.Component {
 			type: 'SET_IS_SIDENAV_FOLDED',
 			isFolded: !config.isSideNavFolded
 		})
-	}
+	} // }}}
 
-	handleTrashedButtonClick(e) {
+	handleTrashedButtonClick(e) { // {{{
 		const { router } = this.context
 		router.push('/trashed')
-	}
+	} // }}}
 
-	handleSwitchFoldersButtonClick() {
-		const { router } = this.context
-		router.push('/home')
-	}
-
-	handleSwitchTagsButtonClick() {
-		const { router } = this.context
-		const { location } = this.props
-
-		router.push({
-			pathname: `/alltags`,
-			query: {
-				key: location.query.key || '',
-				search: '',
-				storage: location.query.storage || ''
-			}
-		})
-	}
-
-	onSortEnd(storage) {
+	onSortEnd(storage) { // {{{
 		return ({ oldIndex, newIndex }) => {
 			const { dispatch } = this.props
 			dataApi
@@ -165,90 +283,88 @@ class SideNav extends React.Component {
 					dispatch({ type: 'REORDER_FOLDER', storage: data.storage })
 				})
 		}
-	}
+	} // }}}
 
-	SideNavComponent(isFolded, storageList) {
-		const { location, data, config } = this.props
+	render() { // {{{
+		const { data, location, config, dispatch } = this.props
+
+		const isFolded = config.isSideNavFolded
+
+		const storageList = data.storageMap.map((storage, key) => {
+			const SortableStorageItem = SortableContainer(StorageItem)
+			return <SortableStorageItem
+				key={storage.key}
+				storage={storage}
+				data={data}
+				location={location}
+				isFolded={isFolded}
+				dispatch={dispatch}
+				onSortEnd={this.onSortEnd.bind(this)(storage)}
+				useDragHandle
+			/>
+		})
+
+		const style = {}
+		if (!isFolded) {
+			style.width = this.props.width
+		}
+
+		const isTagActive = location.pathname.match(/tag/)
+
+		const isFolderMode = !location.pathname.match('/tags') && !location.pathname.match('/alltags')
+
+		return (
+			<div className='SideNav'
+				styleName={isFolded ? 'root--folded' : 'root'}
+				tabIndex='1'
+				style={style}
+			>
+				<div styleName='top'>
+					<div styleName='switch-buttons'>
+						<ListButton onClick={this.handleSwitchFoldersButtonClick.bind(this)} isTagActive={isTagActive} />
+						<TagButton onClick={this.handleSwitchTagsButtonClick.bind(this)} isTagActive={isTagActive} />
+					</div>
+					<div>
+						<PreferenceButton onClick={this.handleMenuButtonClick} />
+					</div>
+				</div>
+				{ isFolderMode ? this.renderFolderMode(isFolded, storageList) : this.renderTagMode(isFolded, storageList) }
+			</div>
+		)
+	} // }}}
+
+	renderFolderMode() { // {{{
+		const { location, data } = this.props
 
 		const isHomeActive = !!location.pathname.match(/^\/home$/)
 		const isStarredActive = !!location.pathname.match(/^\/starred$/)
 		const isTrashedActive = !!location.pathname.match(/^\/trashed$/)
 
-		let component
+		return (
+			<div>
+				<SideNavFilter
+					isFolded={isFolded}
+					isHomeActive={isHomeActive}
+					handleAllNotesButtonClick={(e) => this.handleHomeButtonClick(e)}
+					isStarredActive={isStarredActive}
+					isTrashedActive={isTrashedActive}
+					handleStarredButtonClick={(e) => this.handleStarredButtonClick(e)}
+					handleTrashedButtonClick={(e) => this.handleTrashedButtonClick(e)}
+					counterTotalNote={data.noteMap._map.size - data.trashedSet._set.size}
+					counterStarredNote={data.starredSet._set.size}
+					counterDelNote={data.trashedSet._set.size}
+					handleFilterButtonContextMenu={this.handleFilterButtonContextMenu.bind(this)}
+				/>
 
-		// TagsMode is not selected
-		if (!location.pathname.match('/tags') && !location.pathname.match('/alltags')) {
-			component = (
-				<div>
-					<SideNavFilter
-						isFolded={isFolded}
-						isHomeActive={isHomeActive}
-						handleAllNotesButtonClick={(e) => this.handleHomeButtonClick(e)}
-						isStarredActive={isStarredActive}
-						isTrashedActive={isTrashedActive}
-						handleStarredButtonClick={(e) => this.handleStarredButtonClick(e)}
-						handleTrashedButtonClick={(e) => this.handleTrashedButtonClick(e)}
-						counterTotalNote={data.noteMap._map.size - data.trashedSet._set.size}
-						counterStarredNote={data.starredSet._set.size}
-						counterDelNote={data.trashedSet._set.size}
-						handleFilterButtonContextMenu={this.handleFilterButtonContextMenu.bind(this)}
-					/>
+				<StorageList storageList={storageList} isFolded={isFolded} />
+				<NavToggleButton isFolded={isFolded} handleToggleButtonClick={this.handleToggleButtonClick.bind(this)} />
+			</div>
+		)
+	} // }}}
 
-					<StorageList storageList={storageList} isFolded={isFolded} />
-					<NavToggleButton isFolded={isFolded} handleToggleButtonClick={this.handleToggleButtonClick.bind(this)} />
-				</div>
-			)
-		}
-		else {
-			component = (
-				<div styleName='tabBody'>
-					<div styleName='tag-control'>
-						<div styleName='tag-control-title'>
-							<p>{i18n.__('Tags')}</p>
-						</div>
-						<div styleName='tag-control-sortTagsBy'>
-							<i className='fa fa-angle-down' />
-							<select styleName='tag-control-sortTagsBy-select'
-								title={i18n.__('Select filter mode')}
-								value={config.sortTagsBy}
-								onChange={(e) => this.handleSortTagsByChange(e)}
-							>
-								<option title='Sort alphabetically'
-									value='ALPHABETICAL'>{i18n.__('Alphabetically')}</option>
-								<option title='Sort by update time'
-									value='COUNTER'>{i18n.__('Counter')}</option>
-							</select>
-						</div>
-					</div>
-					<div styleName='tagList'>
-						{this.tagListComponent(data)}
-					</div>
-					<div styleName='tag-control'>
-						<div styleName='tag-control-storage'>
-							<i className='fa fa-angle-down' />
-							<select styleName='tag-control-storage-select'
-								value={location.query.storage}
-								onChange={(e) => this.handleStorageChange(e)}
-							>
-								<option title='All notes' value=''>{i18n.__('All notes')}</option>
-								{data.storageMap.map(storage => [
-									(<option value={`s${storage.key}`}>{storage.name}</option>),
-									...storage.folders.map(folder => (
-										<option value={`f${folder.key}`}>&nbsp; &nbsp; {folder.name}</option>
-									))
-								])}
-							</select>
-						</div>
-					</div>
-				</div>
-			)
-		}
-
-		return component
-	}
-
-	tagListComponent() {
+	renderTagList() { // {{{
 		const { data, location, config } = this.props
+		const { prefix } = this.state
 
 		const query = new TagQuery(location.pathname)
 		const relatedTags = query.listRelatedTags(data.noteMap)
@@ -274,7 +390,7 @@ class SideNav extends React.Component {
 					if (tags[name]) {
 						tags[name].size++
 					}
-					else {
+					else if (!prefix || name[0] === prefix) {
 						const tag = { name, size: 1, related: relatedTags.has(name) }
 
 						tags[name] = tag
@@ -285,11 +401,13 @@ class SideNav extends React.Component {
 			})
 		}
 		else {
-			tagList = data.tagNoteMap.map(
-				(tag, name) => ({ name, size: tag.reduce((acc, note) => acc + (noteMap.get(note).isTrashed ? 0 : 1), 0), related: relatedTags.has(name) })
-			).filter(
-				tag => tag.size > 0
-			)
+			tagList = data.tagNoteMap
+				.map((tag, name) => ({
+					name,
+					size: tag.reduce((acc, note) => acc + (noteMap.get(note).isTrashed ? 0 : 1), 0),
+					related: relatedTags.has(name)
+				}))
+				.filter(tag => tag.size > 0 && (!prefix || tag.name[0] === prefix))
 		}
 
 		query.forEachNegatives((name) => tagList.push({ name, size: 0, related: true }))
@@ -331,154 +449,87 @@ class SideNav extends React.Component {
 				)
 			})
 		)
-	}
+	} // }}}
 
-	handleClickTagListItem(tag) {
-		const { router } = this.context
-		const { location } = this.props
+	renderTagMode() { // {{{
+		const { location, data, config } = this.props
 
-		const query = new TagQuery().addPositiveTag(tag)
+		const prefixes = []
+		if (typeof location.query.storage !== 'undefined' && location.query.storage !== '') {
+			const id = location.query.storage.substr(1)
 
-		query.navigate(location, router)
-	}
-
-	handleClickNarrowToTag(tag, newState) {
-		const { router } = this.context
-		const { location } = this.props
-
-		const query = new TagQuery(location.pathname)
-
-		if(newState === TagState.NEGATIVE) {
-			if(!query.isNegativeTag(tag)) {
-				query.addNegativeTag(tag)
-			}
-		}
-		else if(newState === TagState.POSITIVE) {
-			if(!query.isPositiveTag(tag)) {
-				query.addPositiveTag(tag)
-			}
+			data.noteMap
+				.filter(location.query.storage[0] === 's' ? note => !note.isTrashed && note.storage === id : note => !note.isTrashed && note.folder === id)
+				.forEach(note => {
+					note.tags.forEach(name => {
+						if(name.charCodeAt(0) > 128 && !prefixes.includes(name[0])) {
+							prefixes.push(name[0])
+						}
+					})
+				})
 		}
 		else {
-			if(query.isPositiveTag(tag)) {
-				query.removePositiveTag(tag)
-			}
-			else {
-				query.removeNegativeTag(tag)
-			}
-		}
-
-		query.navigate(location, router)
-	}
-
-	handleSortTagsByChange(e) {
-		const { dispatch } = this.props
-
-		const config = {
-			sortTagsBy: e.target.value
-		}
-
-		ConfigManager.set(config)
-		dispatch({
-			type: 'SET_CONFIG',
-			config
-		})
-	}
-
-	handleStorageChange(e) {
-		const { router } = this.context
-		const { data, location } = this.props
-
-		const storage = e.target.value
-		const id = storage.substr(1)
-		const test = storage[0] === 'f' ? (note) => data.noteMap.get(note).folder === id : (note) => data.noteMap.get(note).storage === id
-
-		let key = location.query.key || ''
-		if (key && !test(key)) {
-			key = ''
-		}
-
-		const query = new TagQuery(location.pathname)
-
-		query.filter((tag) => data.tagNoteMap.get(tag).some((note) => test(note)))
-
-		const pathname = query.toPath()
-
-		router.push({
-			pathname,
-			query: {
-				key,
-				search: location.query.search || '',
-				storage
-			}
-		})
-	}
-
-	emptyTrash(entries) {
-		const { dispatch } = this.props
-		const deletionPromises = entries.map((note) => {
-			return dataApi.deleteNote(note.storage, note.key)
-		})
-		const { confirmDeletion } = this.props.config.ui
-		if (!confirmDeleteNote(confirmDeletion, true)) return
-		Promise.all(deletionPromises)
-			.then((arrayOfStorageAndNoteKeys) => {
-				arrayOfStorageAndNoteKeys.forEach(({ storageKey, noteKey }) => {
-					dispatch({ type: 'DELETE_NOTE', storageKey, noteKey })
-				})
+			data.tagNoteMap.forEach((_, name) => {
+				if(name.charCodeAt(0) > 128 && !prefixes.includes(name[0])) {
+					prefixes.push(name[0])
+				}
 			})
-			.catch((err) => {
-				console.error('Cannot Delete note: ' + err)
-			})
-	}
+		}
 
-	handleFilterButtonContextMenu(event) {
-		const { data } = this.props
-		const trashedNotes = data.trashedSet.toJS().map((uniqueKey) => data.noteMap.get(uniqueKey))
-		context.popup([
-			{ label: i18n.__('Empty Trash'), click: () => this.emptyTrash(trashedNotes) }
-		])
-	}
+		prefixes.sort((a, b) => a.charCodeAt(0) - b.charCodeAt(0))
 
-	render() {
-		const { data, location, config, dispatch } = this.props
-
-		const isFolded = config.isSideNavFolded
-
-		const storageList = data.storageMap.map((storage, key) => {
-			const SortableStorageItem = SortableContainer(StorageItem)
-			return <SortableStorageItem
-				key={storage.key}
-				storage={storage}
-				data={data}
-				location={location}
-				isFolded={isFolded}
-				dispatch={dispatch}
-				onSortEnd={this.onSortEnd.bind(this)(storage)}
-				useDragHandle
-			/>
-		})
-		const style = {}
-		if (!isFolded) style.width = this.props.width
-		const isTagActive = location.pathname.match(/tag/)
 		return (
-			<div className='SideNav'
-				styleName={isFolded ? 'root--folded' : 'root'}
-				tabIndex='1'
-				style={style}
-			>
-				<div styleName='top'>
-					<div styleName='switch-buttons'>
-						<ListButton onClick={this.handleSwitchFoldersButtonClick.bind(this)} isTagActive={isTagActive} />
-						<TagButton onClick={this.handleSwitchTagsButtonClick.bind(this)} isTagActive={isTagActive} />
+			<div styleName='tabBody'>
+				<div styleName='tag-control'>
+					<div styleName='tag-control-prefix'>
+						<i className='fa fa-angle-down' />
+						<select
+							styleName='tag-control-prefix-select'
+							value={this.state.prefix}
+							onChange={(e) => this.handlePrefixChange(e)}
+						>
+							<option value=''>Tags</option>
+							{ prefixes.map(title => <option value={title}>{title}</option>) }
+						</select>
 					</div>
-					<div>
-						<PreferenceButton onClick={this.handleMenuButtonClick} />
+					<div styleName='tag-control-sortTagsBy'>
+						<i className='fa fa-angle-down' />
+						<select
+							styleName='tag-control-sortTagsBy-select'
+							title={i18n.__('Select filter mode')}
+							value={config.sortTagsBy}
+							onChange={(e) => this.handleSortTagsByChange(e)}
+						>
+							<option title='Sort alphabetically'
+								value='ALPHABETICAL'>{i18n.__('Alphabetically')}</option>
+							<option title='Sort by update time'
+								value='COUNTER'>{i18n.__('Counter')}</option>
+						</select>
 					</div>
 				</div>
-				{this.SideNavComponent(isFolded, storageList)}
+				<div styleName='tagList'>
+					{this.renderTagList(data)}
+				</div>
+				<div styleName='tag-control'>
+					<div styleName='tag-control-storage'>
+						<i className='fa fa-angle-down' />
+						<select styleName='tag-control-storage-select'
+							value={location.query.storage}
+							onChange={(e) => this.handleStorageChange(e)}
+						>
+							<option title='All notes' value=''>{i18n.__('All notes')}</option>
+							{data.storageMap.map(storage => [
+								(<option value={`s${storage.key}`}>{storage.name}</option>),
+								...storage.folders.map(folder => (
+									<option value={`f${folder.key}`}>&nbsp; &nbsp; {folder.name}</option>
+								))
+							])}
+						</select>
+					</div>
+				</div>
 			</div>
 		)
-	}
+	} // }}}
 }
 
 SideNav.contextTypes = {
